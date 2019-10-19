098643f11c81fc72ed8b0a62fb78210e658aa85b
megapolis123 <al.averin123@gmail.com>
2019-10-19 20:25:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function zeros(expression) {
   // your solution
+  let arr = (expression.split('*'));
+
+  let typeOfFactor = {
+    'single': []
+    , 'dual': []
+  };
+
+  arr.forEach(elem => {
+    if (elem.endsWith('!!')) {
+      typeOfFactor['dual'].push(parseInt(elem));
+    } else {
+      typeOfFactor['single'].push(parseInt(elem));
+    }
+  })
+
+  let twoAndfive = {
+    'two': 0
+    , 'five': 0
+  };
+
+  typeOfFactor['single'].forEach(elem => {
+    for (let i = elem; i > 0; i--) {
+      let devider = i;
+      for (devider; devider % 5 == 0; devider /= 5) {
+        twoAndfive['five']++;
+      }
+      for (devider; devider % 2 == 0; devider /= 2) {
+        twoAndfive['two']++;
+      }
+    }
+  });
+
+  typeOfFactor['dual'].forEach(elem => {
+    for (let i = elem; i > 0; i -= 2) {
+      let devider = i;
+      for (devider; devider % 5 == 0; devider /= 5) {
+        twoAndfive['five']++;
+      }
+      for (devider; devider % 2 == 0; devider /= 2) {
+        twoAndfive['two']++;
+      }
+    }
+  });
+
+  if (twoAndfive['two'] > twoAndfive['five']) {
+    return twoAndfive['five'];
+  } else {
+    return twoAndfive['two'];
+  }
+
 }
